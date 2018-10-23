db9790a3407f733569a86c23a55606ef586793be
mtalhajamil <talha.jamil@venturedive.com>
2018-10-23 17:48:24 +0500

    some addition

--- src/mvn-demo-project.groovy
+++ src/mvn-demo-project.groovy
@@ -1,6 +1,8 @@
 node{
 	stage('Checkout'){
-		checkout scm 'https://github.com/mtalhajamil/mvn-demo-project'
+		dir(builddir) {
+	    	checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/mtalhajamil/mvn-demo-project']]])
+	    }
 	}
 } 
 pipeline {
@@ -10,7 +12,7 @@ pipeline {
         	agent any
             steps { 
                echo 'maven build after this.'
-               sh 'docker run -i --rm --name ./ -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean install' 
+               sh 'docker run -i --rm --name ./ -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven mvn package -f -e -X' 
             }
         }
     }
