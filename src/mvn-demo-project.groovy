9bf71b9cd3bed439b08d5b190c743aa086e62f3a
mtalhajamil <talha.jamil@venturedive.com>
2018-10-23 16:53:07 +0500

    node addition

--- src/mvn-demo-project.groovy
+++ src/mvn-demo-project.groovy
@@ -6,9 +6,11 @@ node{
 pipeline {
     agent any
     stages { 
-        stage('Build') { 
+        stage('Build') {
+        	agent any
             steps { 
-               echo 'This is a minimal pipeline.' 
+               echo 'maven build after this.'
+               sh 'docker run -it --rm --name ./ -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean install' 
             }
         }
     }
