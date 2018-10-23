265296470120ab1cfd117c10052f27fd19d74205
mtalhajamil <talha.jamil@venturedive.com>
2018-10-23 17:51:31 +0500

    some addition

--- src/mvn-demo-project.groovy
+++ src/mvn-demo-project.groovy
@@ -1,14 +1,14 @@
 import jenkins.model.*
 jenkins = Jenkins.instance
 
+node{
+	stage('Checkout'){
+		dir(builddir) {
+	    	checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/mtalhajamil/mvn-demo-project']]])
+	    }
+	}
+} 
 pipeline {
-	node{
-		stage('Checkout'){
-			dir(builddir) {
-		    	checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/mtalhajamil/mvn-demo-project']]])
-		    }
-		}
-	} 
     agent any
     stages { 
         stage('Build') {
