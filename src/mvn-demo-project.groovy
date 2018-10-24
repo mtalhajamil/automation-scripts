fb7afef663067c2e88b66fc9a2b6f2f6f5dbe6b6
mtalhajamil <talha.jamil@venturedive.com>
2018-10-24 19:19:07 +0500

    update

--- src/mvn-demo-project.groovy
+++ src/mvn-demo-project.groovy
@@ -3,17 +3,18 @@ pipeline {
     stages {
     	stage('Checkout'){
     		steps{
-    			echo 'checkout here'	
-    		}
-			checkout([$class: 'GitSCM', 
-		    branches: [[name: '*/master']],
-		    doGenerateSubmoduleConfigurations: false,
-		    extensions: [
-		        [$class: 'SparseCheckoutPaths',  sparseCheckoutPaths:[[$class:'SparseCheckoutPath', path:'mvn-demo-project/']]]
-		                ],
-		    submoduleCfg: [],
-		    userRemoteConfigs: [[credentialsId: 'mtalhajamil',
-		    url: 'https://github.com/mtalhajamil/mvn-demo-project']]])
+    			echo 'checkout here'
+    			checkout([$class: 'GitSCM', 
+				    branches: [[name: '*/master']],
+				    doGenerateSubmoduleConfigurations: false,
+				    extensions: [
+				        [$class: 'SparseCheckoutPaths',  sparseCheckoutPaths:[[$class:'SparseCheckoutPath', path:'mvn-demo-project/']]]
+				                ],
+				    submoduleCfg: [],
+				    userRemoteConfigs: [[credentialsId: 'mtalhajamil',
+				    url: 'https://github.com/mtalhajamil/mvn-demo-project']]])
+		    		}
+			
 		}
         stage('Build') {
             steps { 
