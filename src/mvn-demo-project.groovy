6708763f67bf337af36ff1a64e520d036b811c99
mtalhajamil <talha.jamil@venturedive.com>
2018-10-26 16:43:32 +0500

    update

--- src/mvn-demo-project.groovy
+++ src/mvn-demo-project.groovy
@@ -51,7 +51,7 @@ pipeline {
                     ).trim()
                 }
                 sh 'docker stop jenkins_tomcat || true && docker rm jenkins_tomcat || true'
-                sh 'docker run -d -e 8080 -p 8080:8080 -v ' + CHECKOUT_DIR + '/target/:/usr/local/tomcat/webapps/ --name jenkins_tomcat tomcat:8'
+                sh 'docker run -d -e 8080 -p 8080:8080 --volumes-from jenkins-data -v ' + CHECKOUT_DIR + '/target/:/usr/local/tomcat/webapps/ --name jenkins_tomcat tomcat:8'
                 sh 'sleep 1'
                 sh 'echo $! > .pidfile'
             }
