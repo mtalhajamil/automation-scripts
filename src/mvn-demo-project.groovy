1736acff3ec6d6cc98b3289243d74721810de2ab
mtalhajamil <talha.jamil@venturedive.com>
2018-10-30 15:15:45 +0500

    update

--- src/mvn-demo-project.groovy
+++ src/mvn-demo-project.groovy
@@ -52,7 +52,7 @@ pipeline {
 //                            returnStdout: true
 //                    ).trim()
 //                }
-                sh 'cp ' + CHECKOUT_DIR + '/target/* /var/war-directory'
+                sh 'cp ' + CHECKOUT_DIR + '/target/*.war /var/war-directory'
                 sh 'docker stop jk_tomcat || true && docker rm jk_tomcat || true'
                 sh 'docker run -d -e 8080 -p 8080:8080 --volumes-from vd_jenkins -v war-directory:/usr/local/tomcat/webapps/ --name jenkins_tomcat tomcat:8'
                 //sh 'sleep 1'
